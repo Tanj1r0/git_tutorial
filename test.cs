e7bd85519cb9c10071d5dbffe9bd2386eff6524f
Olga Nikandrova <keyed-rave.0q@icloud.com>
2023-06-01 17:01:13 +0300

    Start project

---
+++ test.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class MainClass {
+public static void Main (string[] args) {
+// Ввод массива строк с клавиатуры
+Console.Write("Введите массив строк через запятую: ");
+string[] input_array = Console.ReadLine().Split(',');
+
+// Формирование выходного массива строк
+List<string> output_array = new List<string>();
+foreach (string str in input_array) {
+if (str.Trim().Length <= 3) {
+output_array.Add(str.Trim());
+}
+}
+
+// Вывод результата
+Console.WriteLine(string.Join(",", output_array.ToArray()));
+}
+}
